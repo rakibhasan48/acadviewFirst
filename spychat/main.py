bded6b3117e5b9dd9b580be542ffc0c84e7201ad
DESKTOP-VJCDR99\Rakib Hasan <rakib.hasan48@gmail.com>
2017-08-10 00:54:59 +0530

    added some conditionals

--- spychat/main.py
+++ spychat/main.py
@@ -10,7 +10,17 @@ sal = raw_input("Are you a Mr. or Mrs/Ms?  ")
 while len(sal)<1 :
     sal=raw_input('Please enter your title(Mr/Mrs/Ms) : ')
 
-print 'Welcome, {sal} {name}. We are happy to see you.'.format(sal=sal,name=spy_name)
+age = int(raw_input("What is your age {name}? ".format(name=spy_name)))
+if age<18:
+    print("You are still too young to be a spy....come back later ;)")
+    exit()
+elif age>18 and age<60:
+    print 'Welcome, {sal} {name}. We are happy to see you.'.format(sal=sal, name=spy_name)
+else:
+    print("Time to retire, old man :|")
+    exit()
+
+
 
 
 
